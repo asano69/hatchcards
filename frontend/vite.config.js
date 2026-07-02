ed16b5786c68134da133f5d9bd873bf849af6914
asano69 <186772504+asano69@users.noreply.github.com>
2026-07-02 20:35:34 +0900

    tailwind

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import solid from "vite-plugin-solid";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), tailwindcss()],
   server: {
     host: "0.0.0.0",
     port: 3001,
